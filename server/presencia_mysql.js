3c98ffeb8da2c3cbeba2ca9b026f360e4e3181cc
rubencubero <rcubero@cpi-print.es>
2021-02-11 17:43:01 +0100

    Añadida función cierre automático 16h
    Función que se ejecuta cada hora revisando partes pendientes de cierre por llevar activos más de 16 horas.

--- server/presencia_mysql.js
+++ server/presencia_mysql.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dateFormat = require("dateformat");
 const mysql = require("mysql2/promise");
 
 let db = null;
@@ -9,7 +10,7 @@ app.use(express.json());
 
 app.post('/registrar', async(req, res, next)=>{
   const values = req.body.values;
-  console.debug("INSERT INTO presencia (idpersona, idpdt, idpda, inicio,fin, idtlogin, idtlogout, observaciones, DATA, TIME,USER) VALUES (" + values + ");");
+  console.debug('Registrar entrada: INSERT INTO presencia (idpersona, idpdt, idpda, inicio,fin, idtlogin, idtlogout, observaciones, DATA, TIME,USER) VALUES (' + values + ');');
 
   await db.query("INSERT INTO presencia (idpersona, idpdt, idpda, inicio,fin, idtlogin, idtlogout, observaciones, DATA, TIME,USER) VALUES (" + values + ");");
 
@@ -19,10 +20,8 @@ app.post('/registrar', async(req, res, next)=>{
 
 app.post('/cerrar_parte', async(req, res, next)=>{
   const values = req.body.values;
-  console.debug(values);
-
+  console.debug('Registrar salida: ' + values );
   await db.query(values);
-
   res.json({status:"OK"});
   next();
 });
@@ -39,20 +38,19 @@ app.get('/personas/:puntoAcceso', async (req, res, next) => {
                                 "GROUP BY p.id " +
                                 "ORDER BY p.descri;");
   console.info('Consulta ejecutada');
-  res.json(rows);
-  console.info('Res');
+  res.json(rows);  
   next();
-  console.info('next'); 
 });
 
-app.get('/parte_activo/:userID', async (req, res, next) => {        
- const [rows] = await db.query("SELECT id FROM presencia WHERE idpersona = " + req.params.userID + " AND (fin ='' OR fin IS null);");
- console.debug(req.params.userID);
- res.json(rows); 
- next();
+app.get('/parte_activo/:userID', async (req, res, next) => {      
+  console.debug('Buscar parte activo de usuario: ' + req.params.userID);
+  const [rows] = await db.query("SELECT id FROM presencia WHERE idpersona = " + req.params.userID + " AND (fin ='' OR fin IS null);");
+  console.debug('- Resultado: ' + rows[0].id);
+  res.json(rows); 
+  next();
 });
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
@@ -67,7 +65,30 @@ app.use((error, req, res, next) => {
       },
     });
   });
+*/
 
+setInterval( async function (){
+  console.debug('Cerrando partes de +16horas...');
+  const [rows] = await db.query('SELECT pr.id, ' +
+                                'TIMESTAMPDIFF(HOUR, ' + 
+                                'CONCAT(SUBSTR(pr.inicio,1,4),"-", SUBSTR(pr.inicio,5,2),"-", SUBSTR(pr.inicio,7,2)," ", SUBSTR(pr.inicio,10,2),":", SUBSTR(pr.inicio,12,2),":", SUBSTR(pr.inicio,14,2)), ' +
+                                'CURRENT_TIMESTAMP ' +
+                                ') AS horas ' +
+                                'FROM presencia pr WHERE  (fin = "" OR fin IS NULL) GROUP BY pr.id HAVING horas > 15;');
+  console.debug('Partes para cerrar... [' + rows.length + ']');
+  for (i=0;i<rows.length;i++){
+    cerrarParte(rows[i].id, 284, 'Informática: Cierre automático por 16 horas');
+  }
+}, 60000);
+
+async function cerrarParte(id, tipo, observacion){
+  console.debug('- Cerrando parte');
+  console.debug("UPDATE presencia SET fin = '" + dateFormat(Date.now(), "yyyyMMdd hMMss") + "', idtlogout = " + tipo + ", observaciones = '" + observacion + "' WHERE id = " + id + ";");
+  const result = await db.query("UPDATE presencia SET fin = '" + dateFormat(Date.now(), "yyyyMMdd hMMss") + 
+                                "', idtlogout = " + tipo 
+                                + ", observaciones = '" + observacion + "' WHERE id = " + id + ";");
+  console.debug('- Resultado: ' + result.affectedRows + ' fila(s) actualizadas.');
+}
 
 async function main(){
   db = await mysql.createConnection({
@@ -75,6 +96,7 @@ async function main(){
     user: "aspdev",
     password: "11972702",
     database: "eadeptlito"
+    //database: "bp_pruebas"
   });
 
   const server = app.listen(port, () => console.log(`App listening on port: ${port}`));
